080398c8f22b18e393288afab4a3081031a5c80f
sagitc <santos.sagitc@gmail.com>
2025-09-24 14:40:58 -0300

    create home page. Status: layout completed, making buttons functionals.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,68 +3,135 @@
 @section('title', 'Drogarias Camargo | Home')
 
 @push('styles')
-    <link rel="stylesheet" href="{{ Vite::asset('resources/css/pages/home.css') }}">
+<link rel="stylesheet" href="{{ Vite::asset('resources/css/pages/home.css') }}">
 @endpush
 
 @section('content')
-    <section id="main-banner" class="banner1 max-width">
-        <div class="banner1__area"></div>
-    </section>
 
-    <section id="benefits" class="benefits">
-        <div class="benefits__content max-width">
-            <div class="benefits__card">
-                <figure class="benefits__img">
-                    <img src="{{ asset('images/icon_cardPayment_white.svg') }}" alt="Benefício 1">
-                </figure>
+<section id="main-banner">
+    <div class="banner1__area max-width">
+        <div id="banner1__img"></div>
+    </div>
+</section>
 
-                <div class="benefits__text">
-                    <h3 class="benefits__title">Parcelamento</h3>
-                    <p class="benefits__description">Parcelamos suas compras em até 6x sem juros no cartão.</p>
-                </div>
-            </div>
+<section id="benefits">
+    <div class="benefits__content max-width">
+        <div class="benefits__card">
+            <figure class="benefits__img">
+                <img src="{{ asset('images/icon_cardPayment_white.svg') }}" alt="Benefício 1">
+            </figure>
 
-            <div class="benefits__card">
-                <figure class="benefits__img">
-                    <img src="{{ asset('images/icon_delivery_white.svg') }}" alt="Benefício 1">
-                </figure>
-
-                <div class="benefits__text">
-                    <h3 class="benefits__title">Frete grátis</h3>
-                    <p class="benefits__description">Oferecemos frete grátis para compras acima de R$ 100,00.</p>
-                </div>
-            </div>
-
-            <div class="benefits__card">
-                <figure class="benefits__img">
-                    <img src="{{ asset('images/icon_offer_white.svg') }}" alt="Benefício 1">
-                </figure>
-
-                <div class="benefits__text">
-                    <h3 class="benefits__title">Cobrimos ofertas</h3>
-                    <p class="benefits__description">Encontrou um preço melhor? Nós cobrimos!</p>
-                </div>
-            </div>
-
-            <div class="benefits__card">
-                <figure class="benefits__img">
-                    <img src="{{ asset('images/icon_retrieve_white.svg') }}" alt="Benefício 1">
-                </figure>
-
-                <div class="benefits__text">
-                    <h3 class="benefits__title">Retire na loja</h3>
-                    <p class="benefits__description">Retire seu pedido na loja e evite o frete.</p>
-                </div>
+            <div class="benefits__text">
+                <h3 class="benefits__title">Parcelamento</h3>
+                <p class="benefits__description">Parcelamos suas compras em até 6x sem juros no cartão.</p>
             </div>
         </div>
-    </section>
 
-    @include('partials.products_cards')
+        <div class="benefits__card">
+            <figure class="benefits__img">
+                <img src="{{ asset('images/icon_delivery_white.svg') }}" alt="Benefício 1">
+            </figure>
 
-    
+            <div class="benefits__text">
+                <h3 class="benefits__title">Frete grátis</h3>
+                <p class="benefits__description">Oferecemos frete grátis para compras acima de R$ 100,00.</p>
+            </div>
+        </div>
+
+        <div class="benefits__card">
+            <figure class="benefits__img">
+                <img src="{{ asset('images/icon_offer_white.svg') }}" alt="Benefício 1">
+            </figure>
+
+            <div class="benefits__text">
+                <h3 class="benefits__title">Cobrimos ofertas</h3>
+                <p class="benefits__description">Encontrou um preço melhor? Nós cobrimos!</p>
+            </div>
+        </div>
+
+        <div class="benefits__card">
+            <figure class="benefits__img">
+                <img src="{{ asset('images/icon_retrieve_white.svg') }}" alt="Benefício 1">
+            </figure>
+
+            <div class="benefits__text">
+                <h3 class="benefits__title">Retire na loja</h3>
+                <p class="benefits__description">Retire seu pedido na loja e evite o frete.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="products-cards" class="products">
+    <div class="products__area max-width">
+        @include('partials.section_top', ['section_title' => 'Produtos em destaque'])
+        <div class="products__content">
+            @include('partials.products_cards')
+        </div>
+    </div>
+</section>
+
+<section id="banner2">
+    <div class="banner2__area max-width">
+        <div id="banner2__img"></div>
+    </div>
+</section>
+
+<section id="catalog">
+    <div class="catalog__area max-width">
+        <h2 class="catalog__title">Compre por categoria</h2>
+
+        <div class="catalog__btn-options">
+            <button class="catalog__option active">
+                Dor de cabeça
+                <span class="mobile-touch"></span>
+            </button>
+            <button class="catalog__option">
+                Dor muscular
+                <span class="mobile-touch"></span>
+            </button>
+            <button class="catalog__option">
+                Cólica
+                <span class="mobile-touch"></span>
+            </button>
+            <button class="catalog__option">
+                Gripe e catarro
+                <span class="mobile-touch"></span>
+            </button>
+            <button class="catalog__option">
+                Febre
+                <span class="mobile-touch"></span>
+            </button>
+            <button class="catalog__option">
+                Dor na garganta
+                <span class="mobile-touch"></span>
+            </button>
+        </div>
+
+        <div class="catalog__product-content">
+            @include('partials.products_cards')
+        </div>
+    </div>
+</section>
+
+<section id="banner3">
+    <div class="banner3__area max-width">
+        <div class="banner3__img"></div>
+        <div class="banner3__img"></div>
+    </div>
+</section>
+
+<section id="daily-products" class="daily">
+    <div class="daily__area max-width">
+        @include('partials.section_top', ['section_title' => 'Promoções diárias'])
+        <div class="daily__content">
+            @include('partials.products_cards')
+        </div>
+    </div>
+</section>
 
 @endsection
 
 @section('footer')
-    @include('partials.footer')
+@include('partials.footer')
 @endsection
---
+++ resources/views/partials/section_top.blade.php
@@ -0,0 +1,15 @@
+<div class="products__top">
+    <h2 class="products__section-title">{{ $section_title }}</h2>
+
+    <div class="products__nav" role="navigation" aria-label="Navegação entre os produtos">
+        <button disabled class="products__btn-nav products__btn--prev" aria-label="Botão para navegar para os produtos anteriores">
+            <img src="{{ asset('images/icon_arrow-left_secundary.svg') }}" alt="Ícone de seta para esquerda">
+            <span class="mobile-touch"></span>
+        </button>
+
+        <button class="products__btn-nav products__btn--next" aria-label="Botão para navegar para os próximos produtos">
+            <img src="{{ asset('images/icon_arrow-right_secundary.svg') }}" alt="Ícone de seta para direita">
+            <span class="mobile-touch"></span>
+        </button>
+    </div>
+</div>
